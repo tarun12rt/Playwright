c7b70fbb467e4327a838d76d60873cc4c0387ac0
Tarun Kumar <104968735+tarun12rt@users.noreply.github.com>
2026-01-17 16:49:49 +0530

    scripting for loginpage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,22 @@
 package pages;
 
-public class LoginPage {
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+public class LoginPage extends BasePage {
+
+    private final Locator sample;
+
+    public LoginPage(Page page) {
+        super(page); // ✅ VERY IMPORTANT
+        this.sample = page.getByText("sample");
+    }
+
+    public void open() {
+        openBaseUrl();
+    }
+
+    public boolean clickProfileIconAndVerifyLoginPage() {
+        return true;
+    }
 }
